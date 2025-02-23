1f9cfd346da1d152c92a7142db06fbd6ea4cc021
Johan Berggren <jberggren@gmail.com>
2025-02-23 12:17:52 +0100

    Add license header

--- src/healthz.py
+++ src/healthz.py
@@ -32,7 +32,7 @@ def _check_posgresql_connection():
     """
     try:
         db = database.SessionLocal()
-        db.execute(text('SELECT 1'))
+        db.execute(text('SELECT foo'))
         return "Ok"
     except Exception:  # pylint: disable=broad-except
         return "Database connection error"
